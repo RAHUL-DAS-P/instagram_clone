f95c18e319ba7da04f8314097e88527ef653be67
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-07-18 14:54:07 +0530

    firebase

---
+++ lib/responsive/resposive_layout_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+  const ResponsiveLayout({
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
