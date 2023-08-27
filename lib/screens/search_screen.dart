42522f3e166bc48b6f9c82d7806281baffa42e00
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-08-27 19:46:34 +0530

    final before fix

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -73,7 +74,17 @@ class _SearchScreenState extends State<SearchScreen> {
                     child: CircularProgressIndicator(),
                   );
                 }
-                return 
+                return MasonryGridView.count(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  itemBuilder: (context, index) => StaggeredGridTile.count(
+                    crossAxisCellCount: (index % 7 == 0) ? 2 : 1,
+                    mainAxisCellCount: (index % 7 == 0) ? 2 : 1,
+                    child: Image.network(
+                        (snapshot.data! as dynamic).docs[index]["photoUrl"]),
+                  ),
+                );
               },
             ),
     );
