5642aca53b6eb6874e040be5bd8df9131889373f
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-08-27 16:45:00 +0530

    created the search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  bool _isUsers = false;
+  final TextEditingController cont = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    cont.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            controller: cont,
+            decoration: const InputDecoration(labelText: "Serach for a user"),
+            onFieldSubmitted: (String username) {
+              setState(
+                () {
+                  _isUsers = true;
+                },
+              );
+            },
+          ),
+        ),
+        body: _isUsers
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection("users")
+                    .where(
+                      "username",
+                      isGreaterThanOrEqualTo: cont.text,
+                    )
+                    .get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return ListView.builder(
+                    itemBuilder: (context, index) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            (snapshot.data! as dynamic).docs[index]
+                                ['photoUrl']),
+                      ),
+                      title: Text(
+                          (snapshot.data! as dynamic).docs[index]["username"]),
+                    ),
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                  );
+                },
+              )
+            : Center(
+                child: Text("posts"),
+              ));
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:instagram_clone/screens/add_post.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 int webScreenSize = 600;
 
 final List<Widget> widgetOptions = <Widget>[
   FeedScreen(),
-  const Text("Ticket"),
+  SearchScreen(),
   const AddPost(),
   const Text("hai"),
 ];
